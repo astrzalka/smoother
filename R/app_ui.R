4431ee3edcf28171e188d8f5e08be858cce485da
agnieszka <the.larch19@gmail.com>
2020-09-28 13:42:47 +0200

    english translation

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,65 +10,64 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # List the first level UI elements here 
     navbarPage("smoother",
-               selected = "Dodaje linię trendu do wykresu fluorescencji",
+               selected = "Adds trend line to a fluorescence profile",
                theme = shinythemes::shinytheme("united"),
                tags$head(
                  tags$style(HTML("hr {border-top: 1px solid #000000;}"))
                ),
                tabPanel(
                  # Panel title
-                 "Dodaje linię trendu do wykresu fluorescencji",
+                 "Adds trend line to a fluorescence profile",
                  
                  sidebarLayout(
                    sidebarPanel(
-                     checkboxInput('example', 'Czy chcesz załadować przykładowe dane?'),
-                     fileInput("dane_1", 'Wybierz pierwszy plik .txt',
+                     checkboxInput('example', 'Load example dataset?'),
+                     fileInput("dane_1", 'Choose first txt file',
                                accept=c('.txt')),
-                     fileInput("dane_2", 'Wybierz drugi plik .txt',
+                     fileInput("dane_2", 'Choose second txt file',
                                accept=c('.txt')),
-                     radioButtons('header', 'Czy dane mają nagłówki?', choices = list("Tak" = TRUE, "Nie" = FALSE), 
+                     radioButtons('header', 'Do datasets contain headers?', choices = list("Yes" = TRUE, "No" = FALSE), 
                                   inline = TRUE),
                      radioButtons('sep', 'Separator?',
-                                  c(Przecinek=',',
-                                    Średnik=';',
+                                  c(Comma=',',
+                                    Semicolon=';',
                                     Tabulator='\t', 
-                                    Spacja = " "),
+                                    Space = " "),
                                   "\t", inline = TRUE),
-                     radioButtons("procent_x", "Czy zmienić dlugość komórki na procenty", 
-                                  choices = list("Tak" = TRUE, "Nie" = FALSE), 
+                     radioButtons("procent_x", "Change cell length into percent?", 
+                                  choices = list("Yes" = TRUE, "No" = FALSE), 
                                   selected = TRUE, inline = TRUE),
-                     radioButtons("procent_y", "Czy zmienić intensywność fluorescencji na procenty", 
-                                  choices = list("Tak" = TRUE, "Nie" = FALSE), 
+                     radioButtons("procent_y", "Change fluorescence intensity into percent?", 
+                                  choices = list("Yes" = TRUE, "No" = FALSE), 
                                   selected = TRUE, inline = TRUE),
-                     radioButtons('jaki_wykres', "Jaki wykres narysować?", 
-                                  choices = list('Pierwsze 10 komórek' = 'example', 
-                                                 'Wszystkie nałożone jako linie + linia trendu' = 'linie',
-                                                 'Wszystkie nałożone jako punkty + linia trendu' = 'punkty',
-                                                 'Tylko linie trendu' = 'trend')),
-                     radioButtons('jaki_trend', "Jak wyliczyć linię trendu?", 
-                                  choices = list('Loess (ze stat_smooth)' = 'loess',
-                                                 'Średnia' = 'mean',
-                                                 'Średnia ruchoma' = 'rollmean'),
+                     radioButtons('jaki_wykres', "Choose plot type:", 
+                                  choices = list('First 10 cells' = 'example', 
+                                                 'All profiles as lines with a trend line' = 'linie',
+                                                 'All profiles as points with a trend line' = 'punkty',
+                                                 'Only trend lines' = 'trend')),
+                     radioButtons('jaki_trend', "How should trend line be calculated?", 
+                                  choices = list('Loess (from stat_smooth)' = 'loess',
+                                                 'Mean' = 'mean',
+                                                 'Rolling mean' = 'rollmean'),
                                   selected = 'loess', inline = TRUE),
-                     numericInput("alpha", "Podaj wartość alpha dla linii/punktów na wykresie", 0.3, 
+                     numericInput("alpha", "Choose alpha value for points or lines", 0.3, 
                                   min = 0, max = 1, step = 0.1),
-                     numericInput("trend_size", "Podaj grubość linii trendu", 1, 
+                     numericInput("trend_size", "Choose size of trend line", 1, 
                                   min = 0, max = 10, step = 0.1),
-                     textInput('wlasne_kolory', 'Tutaj wpisz wybrane nazwy kolorów oddzielając je przecinkiem. Powinny być to kolory 
-                                          predefiniowane w R (można sprawdzić jakie np. na stronie 
-                                          http://sape.inf.usi.ch/quick-reference/ggplot2/colour) albo skorzystać 
-                                          z notacji #FF0000',  ''),
-                     radioButtons("czy_legenda", "Czy dodać legendę do wykresu", 
-                                  choices = list("Tak" = "TRUE", "Nie" = "FALSE"), 
+                     textInput('wlasne_kolory', 'Please provide color names for custom scale separated by commas. 
+                                                    Color names should be predefined in R (check:  http://sape.inf.usi.ch/quick-reference/ggplot2/colour) or use
+                                                    #FF0000 format',  ''),
+                     radioButtons("czy_legenda", "Add legend?", 
+                                  choices = list("Yes" = "TRUE", "No" = "FALSE"), 
                                   selected = "TRUE", inline = TRUE),
-                     textInput('legenda_nazwa', 'Tytuł legendy', 'Białko'),
-                     textInput('legenda_grupy', 'Podaj nazwy grup do legendy oddzielone przecinkiem', 'x, y'),
-                     textInput('os_x_nazwa', 'Nazwa osi X', '% długości komórki'),
-                     textInput('os_y_nazwa', 'Nazwa osi Y', '% intensywności fluorescencji'),
-                     downloadButton('download_wykres', 'Pobierz wykres (dodaj .png do nazwy pliku)'),
-                     numericInput('width', 'Szerokość obrazka [cm]', 20, min = 5, max = 25),
-                     numericInput('height', 'Wysokość obrazka [cm]', 14, min = 5, max = 25),
-                     numericInput('res', 'Rozdzielczość', 200, min = 100, max = 500)
+                     textInput('legenda_nazwa', 'Legend title', 'Protein'),
+                     textInput('legenda_grupy', 'Please provide names for legend groups separated by commas', 'x, y'),
+                     textInput('os_x_nazwa', 'X axis name', '% of cell length'),
+                     textInput('os_y_nazwa', 'Y axis name', '% of fluorescence intensity'),
+                     downloadButton('download_wykres', 'Download plot'),
+                     numericInput('width', 'Plot width [cm]', 20, min = 5, max = 25),
+                     numericInput('height', 'Plot height [cm]', 14, min = 5, max = 25),
+                     numericInput('res', 'Resolution', 200, min = 100, max = 500)
                      
                      
                    ),
@@ -76,14 +75,14 @@ app_ui <- function(request) {
                    # Show a plot 
                    mainPanel(
                      tabsetPanel(type = "tabs",
-                                 tabPanel("Wykres",
+                                 tabPanel("Plot",
                                           plotOutput("wykres", height = "700px")
                                  ),
                                  #br(),
                                  #hr(),
                                  #br(),
-                                 tabPanel("Dane",
-                                          h4("Pierwsze 6 wierszy wczytanego pliku, kolumna grupa i nazwy kolumn są dodawane automatycznie"),
+                                 tabPanel("Data",
+                                          h4("First six lines of uploaded dataset, group column and column names are added automatically"),
                                           tableOutput("dane")
                                  )
                      )
@@ -93,28 +92,28 @@ app_ui <- function(request) {
                tabPanel("kmeans",
                         sidebarLayout(
                           sidebarPanel(
-                            radioButtons('kmeans', 'Jaki algorytm zastosować?', 
+                            radioButtons('kmeans', 'Choose algorithm?', 
                                          choices = list('kmeans' = 'kmeans', 'fuzzy kmeans' = 'fuzzy'), 
                                          inline = TRUE),
-                            numericInput('n_clusters', "Podaj liczbę grup", min = 2, step = 1, value = 2),
-                            downloadButton('download_wykres_kmeans', 'Pobierz wykres (dodaj .png do nazwy pliku)'),
-                            numericInput('width_kmeans', 'Szerokość obrazka [cm]', 20, min = 5, max = 25),
-                            numericInput('height_kmeans', 'Wysokość obrazka [cm]', 14, min = 5, max = 25),
-                            numericInput('res_kmeans', 'Rozdzielczość', 200, min = 100, max = 500)
+                            numericInput('n_clusters', "Number of groups", min = 2, step = 1, value = 2),
+                            downloadButton('download_wykres_kmeans', 'Download plot'),
+                            numericInput('width_kmeans', 'Plot width [cm]', 20, min = 5, max = 25),
+                            numericInput('height_kmeans', 'Plot height [cm]', 14, min = 5, max = 25),
+                            numericInput('res_kmeans', 'Resolution', 200, min = 100, max = 500)
                           ),
                           mainPanel(
                             tabsetPanel(
-                              tabPanel('Wykres - linia trendu',
+                              tabPanel('Plot - trend line',
                                        plotOutput("wykres_kmeans", height = "700px")
                               ),
-                              tabPanel('Wykres - grupy',
+                              tabPanel('Plot - groups',
                                        plotOutput("wykres_cluster", height = "500px"),
                                        plotOutput("wykres_grupy", height = '300px')
                               ),
-                              tabPanel('Podsumowanie',
+                              tabPanel('Summary',
                                        verbatimTextOutput('podsumowanie_cluster')
                                        ),
-                              tabPanel('Dane',
+                              tabPanel('Data',
                                        tableOutput("dane_kmeans")
                               )
                             )
